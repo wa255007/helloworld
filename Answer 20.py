06daff1fa42760fc4bbb9c66c0765bbca380bcf2
WA255007 <wasif.abac.1@gmail.com>
2021-02-23 12:26:10 +0800

    updating file 20 for new branch

--- Answer 20.py
+++ Answer 20.py
@@ -4,11 +4,11 @@
 def myfunc(inpu=100):
     for num in range(1,inpu+1):
         if num % 3 == 0 and num % 5 == 0:
-           print ("FB" , end=" "),
+           print ("FizzBuzz" , end=" "),
         elif num % 3 == 0:
-           print  ("F" , end=" "),
+           print  ("Fizz" , end=" "),
         elif num % 5 == 0:
-           print ("B" , end=" ") ,
+           print ("Buzz" , end=" ") ,
         else:
             print (num , end=" ")
 
@@ -16,3 +16,6 @@ def myfunc(inpu=100):
 # function call 
 myfunc()  or myfunc(100)
 
+
+###########3updating this file in new branch will push to branch and then 
+####merge with master brach via pull request
